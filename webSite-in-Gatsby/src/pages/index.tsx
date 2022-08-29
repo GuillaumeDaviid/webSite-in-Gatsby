4069d6afc6a0e626df80b9f784bccb327e96e206
Guillaume David <guillaumedaavidpro@gmail.com>
2022-08-29 15:04:57 +0200

    add About component

--- webSite-in-Gatsby/src/pages/index.tsx
+++ webSite-in-Gatsby/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Header from '../components/Header'
+import About from '../components/About'
 import type { HeadFC } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
       <Header />
+      <About />
     </div>
   )
 }
