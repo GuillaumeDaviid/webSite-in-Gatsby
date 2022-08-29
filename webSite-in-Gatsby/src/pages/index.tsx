7299563b071ebabd5b46f6a75e8c4243c684233e
Guillaume David <guillaumedaavidpro@gmail.com>
2022-08-29 15:03:25 +0200

    add Header component

--- webSite-in-Gatsby/src/pages/index.tsx
+++ webSite-in-Gatsby/src/pages/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react"
+import Header from '../components/Header'
 import type { HeadFC } from "gatsby"
 
 const IndexPage = () => {
   return (
     <div>
-
+      <Header />
     </div>
   )
 }
