534c48c08f5c70cb8c117dd87108f82eeb7a7537
Guillaume David <guillaumedaavidpro@gmail.com>
2022-08-31 15:23:29 +0200

    install gatsby-image

--- webSite-in-Gatsby/gatsby-config.ts
+++ webSite-in-Gatsby/gatsby-config.ts
@@ -1,5 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
+const path = require(`path`)
+
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `webSite-in-Gatsby`,
@@ -9,7 +11,13 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass", `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `images`,
+      path: path.join(__dirname, `src`, `images`),
+    },
+  },]
 };
 
 export default config;
