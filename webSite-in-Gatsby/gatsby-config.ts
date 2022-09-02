a583f81d53619813caecd32353b5b2e3a261f0ca
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-02 15:50:51 +0200

    install mdx plugin

--- webSite-in-Gatsby/gatsby-config.ts
+++ webSite-in-Gatsby/gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", 'gatsby-plugin-image', `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, {
+  plugins: ["gatsby-plugin-sass", 'gatsby-plugin-image', `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, 'gatsby-plugin-mdx', {
     resolve: `gatsby-source-filesystem`,
     options: {
       name: `images`,
---
+++ webSite-in-Gatsby/src/pages/blog/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+
+const BlogPage = () => {
+
+  return (
+    <div>
+      Hello 
+    </div>
+      
+  )
+}
+
+
+export default BlogPage
