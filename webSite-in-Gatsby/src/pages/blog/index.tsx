586fd77a6732fb8de9469317e152303083ba4354
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-06 11:58:02 +0200

    show blog post

--- webSite-in-Gatsby/src/pages/blog/index.tsx
+++ webSite-in-Gatsby/src/pages/blog/index.tsx
@@ -1,15 +1,38 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({data}:any) => {
 
   return (
     <div>
-      Hello 
+      {
+        data.allMdx.nodes.map((node:any) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
+        ))
+      } 
     </div>
       
   )
 }
+
+export const query = graphql`
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      excerpt
+    }
+  }
+}
+`
 
 
 export default BlogPage
