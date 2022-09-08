b6cd06df1d921e45191e5fffdb2a25cd1e04a30c
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-08 14:35:50 +0200

    Layout style

--- webSite-in-Gatsby/src/components/Layout.tsx
+++ webSite-in-Gatsby/src/components/Layout.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
+import './styles/Layout.scss'
 
 const Layout = ({ pageTitle, children }:any) => {
 
     return(
-        <div>
+        <div className='Layout'>
+          <div className='Layout__nav'>
             <nav>
-        <ul >
-          <li ><Link to="/" >Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li >
-            <Link to="/blog">
-              Blog
-            </Link>
-          </li>
+        <ul className='Layout__nav--children' >
+          <li className='nav__elt'><Link to="/" >Home</Link></li>
+          <li className='nav__elt'><Link to="/about">About</Link></li>
+          <li className='nav__elt'><Link to="/blog">Blog</Link></li>
         </ul>
       </nav>
+      <h1 className='Layout__title'>{pageTitle}</h1>
+      <div className='Header_separator-white'></div>
+      </div>
       <main>
-        <h1>{pageTitle}</h1>
         {children}
       </main>
         </div>
