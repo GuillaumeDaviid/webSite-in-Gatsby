999926d40c18b71fae93cecda4e6f986c73e72c5
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-12 15:57:49 +0200

    add hero image to post

--- webSite-in-Gatsby/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ webSite-in-Gatsby/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
 import type { HeadFC } from "gatsby"
+import { GatsbyImage } from 'gatsby-plugin-image'
 import Layout from '../../components/Layout'
 // @ts-ignore
 import { MDXRenderer } from 'gatsby-plugin-mdx'
@@ -9,20 +10,28 @@ import { MDXRenderer } from 'gatsby-plugin-mdx'
 const BlogPost = ({ data, children }:any) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
+      <p>Guillaume David - {data.mdx.frontmatter.date}</p>
+      <GatsbyImage image={data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData} alt={data.mdx.frontmatter.hero_image_alt} />
       {children}
     </Layout>
   )
 }
+
 export const query = graphql`
-  query ($id: String) {
-    mdx(id: {eq: $id}) {
-      frontmatter {
-        title
-        date(formatString: "MMMM D, YYYY")
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "MMMM D, YYYY")
+      hero_image_alt
+      hero_image {
+      childImageSharp {
+        gatsbyImageData(width: 400)
       }
     }
+    }
   }
+}
 `
 
 export const Head: HeadFC = () => <title>Home Page</title>
