50290b3d2db9cdda78404534fcdbcadd48503470
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-12 16:11:08 +0200

    style articles

--- webSite-in-Gatsby/src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ webSite-in-Gatsby/src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -3,16 +3,19 @@ import { graphql } from 'gatsby'
 import type { HeadFC } from "gatsby"
 import { GatsbyImage } from 'gatsby-plugin-image'
 import Layout from '../../components/Layout'
-// @ts-ignore
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import '../styles/mdx.scss'
 
 
 const BlogPost = ({ data, children }:any) => {
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
+      <div className="mdx">
       <p>Guillaume David - {data.mdx.frontmatter.date}</p>
-      <GatsbyImage image={data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData} alt={data.mdx.frontmatter.hero_image_alt} />
+      <GatsbyImage image={data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData} alt={data.mdx.frontmatter.hero_image_alt} className="mdx_img" />
+      <div className='mdx_content'>
       {children}
+      </div>
+      </div>
     </Layout>
   )
 }
