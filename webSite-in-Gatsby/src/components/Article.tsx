2bb23be9ace2f30e316afe8f789b0135e219bbf8
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-15 10:34:55 +0200

    staticQuery in article component

--- webSite-in-Gatsby/src/components/Article.tsx
+++ webSite-in-Gatsby/src/components/Article.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
 import {useState} from 'react'
 import './styles/Article.scss'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
 
-export default function Article() {
+export default function Article({data}:any) {
   const [p, setp] = useState(0);
   const [nbr, setNbr] = useState(1);
+
+  console.log(data)
 
   function handleClickNext() {
     let carrousel = document.getElementById('carrousel');
@@ -47,7 +51,59 @@ function handleClickPrevious() {
 
                 <div className='Article_items-elt' id="carrousel">
 
-                
+                {/*
+           data.allMdx.nodes.map((node:any) => (
+              <article key={node.id} className="blog_article">
+            <GatsbyImage image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData} alt='asdsf' className='blog_image'/>
+            <p>{node.frontmatter.date}</p>
+            <h2 className="blog_title">
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p className='blog_excerpt'>{node.excerpt}</p>
+          </article>
+        ))
+           */ }
+
+<StaticQuery
+      query={graphql`
+      query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+          nodes {
+            frontmatter {
+              date(formatString: "MMMM D, YYYY")
+              title
+              slug
+              featuredImage {
+                childImageSharp {
+                  gatsbyImageData(width: 250)
+                }
+              }
+            }
+            id
+            excerpt
+          }
+        }
+      }
+      `}
+      render={data => (
+        <div>
+          <div>{data.allMdx.nodes.map((node:any) => (
+              <article key={node.id} className="blog_article">
+            <GatsbyImage image={node.frontmatter.featuredImage.childImageSharp.gatsbyImageData} alt='asdsf' className='blog_image'/>
+            <p>{node.frontmatter.date}</p>
+            <h2 className="blog_title">
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p className='blog_excerpt'>{node.excerpt}</p>
+          </article>
+        ))}</div>
+        </div>
+      )}
+    />
 
                 </div>
                 
