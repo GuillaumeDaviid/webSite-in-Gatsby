62dd381bd84a29291e3958d9ac0e11d89132979b
Guillaume David <guillaumedaavidpro@gmail.com>
2022-09-19 16:23:21 +0200

    install and try to add image in post but not working + style for last post

--- webSite-in-Gatsby/gatsby-config.ts
+++ webSite-in-Gatsby/gatsby-config.ts
@@ -11,7 +11,24 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", 'gatsby-plugin-image', `gatsby-transformer-sharp`, `gatsby-plugin-sharp`, 'gatsby-plugin-mdx', {
+  plugins: ["gatsby-plugin-sass", 'gatsby-plugin-image', `gatsby-transformer-sharp`, `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 1200,
+          },
+        },
+      ],
+    },
+  },
+  'gatsby-plugin-mdx', {
     resolve: `gatsby-source-filesystem`,
     options: {
       name: `images`,
