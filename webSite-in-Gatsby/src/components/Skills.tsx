3fd443b16427c56cf4fd9c652e182ebf06728c9d
Guillaume David <guillaumedaavidpro@gmail.com>
2022-10-03 16:42:07 +0200

    Skills component unit tests

--- webSite-in-Gatsby/src/components/Skills.tsx
+++ webSite-in-Gatsby/src/components/Skills.tsx
@@ -5,7 +5,7 @@ export default function Skill() {
     return(
         <div>
             <div className="project">
-                    <div className='project_block'>
+                    <div className='project_block' data-testid="project">
 
                     
                     <a className='project_btn' target="_blank">VOTRE PROJET</a>
@@ -16,24 +16,24 @@ export default function Skill() {
 
             <div className='Skill'>
                
-            <h2>COMPETENCES</h2>
-            <div className='Skill_separator'></div>
+            <h2 data-testid="title">COMPETENCES</h2>
+            <div className='Skill_separator' data-testid="separator"></div>
             
 
             <div className='Skill_skills'>
-                <div className='skills'>HTML/CSS</div>
-                <div className='skills'>SASS/SCSS</div>
-                <div className='skills'>TypeScript</div>
-                <div className='skills'>React.JS</div>
-                <div className='skills'>NodeJS</div>
-                <div className='skills'>NextJS</div>
+                <div className='skills' data-testid="skills">HTML/CSS</div>
+                <div className='skills' data-testid="skills">SASS/SCSS</div>
+                <div className='skills' data-testid="skills">TypeScript</div>
+                <div className='skills' data-testid="skills">React.JS</div>
+                <div className='skills' data-testid="skills">NodeJS</div>
+                <div className='skills' data-testid="skills">NextJS</div>
 
-                <div className='skills'>Javascript</div>
-                <div className='skills'>SQL</div>
-                <div className='skills'>PHP</div>
-                <div className='skills'>Python</div>
-                <div className='skills'>MySQL</div>
-                <div className='skills'>Gatsby</div>
+                <div className='skills' data-testid="skills">Javascript</div>
+                <div className='skills' data-testid="skills">SQL</div>
+                <div className='skills' data-testid="skills">PHP</div>
+                <div className='skills' data-testid="skills">Python</div>
+                <div className='skills' data-testid="skills">MySQL</div>
+                <div className='skills' data-testid="skills">Gatsby</div>
             </div>
             </div>
 
---
+++ webSite-in-Gatsby/src/components/__tests__/Skills.js
@@ -0,0 +1,41 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Skills from "../Skills.tsx"
+import { render, screen } from "@testing-library/react"
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Skills />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("should have a title element", () => {
+    render(<Skills />)
+
+    const title = screen.getByTestId('title');
+    expect(title).toBeTruthy();
+  })
+
+  it("should have a project block", () => {
+    render(<Skills />)
+
+    const project = screen.getByTestId('project');
+    expect(project).toBeTruthy();
+  })
+
+  it("should have a separator element", () => {
+    render(<Skills />)
+
+    const separator = screen.getByTestId('separator');
+    expect(separator).toBeTruthy();
+  })
+
+  it("should have 12 skills elements", () => {
+    render(<Skills />)
+
+    const skills = screen.getAllByTestId('skills');
+    expect(skills).toHaveLength(12);
+  })
+})
