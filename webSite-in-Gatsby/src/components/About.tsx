51b4f46d362b2c3f06d89549f280b622cc20f61b
Guillaume David <guillaumedaavidpro@gmail.com>
2022-10-03 16:14:01 +0200

    About component unit tests

--- webSite-in-Gatsby/src/components/About.tsx
+++ webSite-in-Gatsby/src/components/About.tsx
@@ -6,13 +6,13 @@ export default function About() {
         <section id="introduction">
         <div className="About">
 
-            <h2 className="About_title">A PROPOS</h2>
-            <div className="About_separator"></div>
-            <p className="About_text">Développeur web <strong>Front-End</strong> Freelance dans les Hauts-de-France (Lille), 
+            <h2 className="About_title" data-testid="title">A PROPOS</h2>
+            <div className="About_separator" data-testid="separator"></div>
+            <p className="About_text" data-testid="text">Développeur web <strong>Front-End</strong> Freelance dans les Hauts-de-France (Lille), 
                 Spécialisé <strong>Javascript / React.js</strong> et dans l&apos;intégration /
                 développement responsive <strong>HTML/CSS/JS</strong>. Je vous accompagne dans le développement de votre projet web. Vous retrouverez ici mon portfolio et un moyen de me contacter.</p>
 
-                <div className="creativity">
+                <div className="creativity" data-testid="creativity">
                     <div className='creativity_block'>
 
                     
