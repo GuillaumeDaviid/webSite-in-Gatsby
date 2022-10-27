6ef396e2f83bd03edec415773e8cf0ff1a7ffd86
Guillaume David <guillaumedaavidpro@gmail.com>
2022-10-27 11:18:29 +0200

    create data.ts

---
+++ webSite-in-Gatsby/src/pages/portfolio/data.ts
@@ -0,0 +1,69 @@
+export const productList = [
+	{
+        name: "Netflix",
+		title: 'Netflix - Clone Netflix',
+		description: "Réalisation d'un clone du site web de Netflix dans le cadre d'un projet personnel",
+		id: '1',
+		technique: `Projet réalisé avec le framework React.js et le préprocesseur Sass (SCSS). J'ai réalisé ce projet dans le cadre d'un travail personnel.
+                    
+        Taches : 
+         - Développement des fonctionnalités avec React.JS
+         - Intégration responsive SASS (SCSS)
+         - Axios / React-router / Material-UI
+         - Test avec React-Testing Library et JEST
+         - CI/CD avec Heroku et Github`,
+		technologies: ["React.js", "Javascript", "SASS (SCSS)", "Axios", "React Router", "Material-UI", "React-Testing-Library / JEST", "Heroku"],
+		link: "https://clone-guillaumedavid.herokuapp.com/",
+	},
+
+    {
+        name: "01 Academie",
+		title: '01 Academie',
+		description: "Création d'un site web de quizz qui permet de tester ses connaissances en programmation.",
+		id: '2',
+		technique: `Plusieurs version du projet, une avec du Javascript Vanilla et une autre avec React.JS. Backend réalisé en PHP.
+                    
+        Taches : 
+         - Intégration Responsive
+         - Optimisation SEO
+         - Développement de l'application avec React.JS 
+         - Développement API REST avec PHP
+         - Base de donnée SQL / MySQL
+         - Tests unitaire JEST / React Testing Library`,
+		technologies: ["React.js", "Javascript", "SASS (SCSS)", "Axios", "React Router", "PHP", "SQL / MySQL", "React-Testing-Library / JEST", "API REST", "OVH"],
+		link: "https://github.com/GuillaumeDaviid/01-Academie-Version-React",
+	},
+
+    {
+        name: "Jeu de Dame",
+		title: 'Jeu de Dame',
+		description: "Création d'un jeu de Dame dans le cadre d'un projet personnel.",
+		id: '3',
+		technique: `Plusieurs version du projet, une avec du Javascript Vanilla et une autre avec React.JS. Backend réalisé en PHP.
+                    
+        Taches : 
+         - Intégration Responsive
+         - Optimisation SEO
+         - Développement du jeu en Javascript Vanilla
+         - Déploiement via Github
+         - Test unitaire avec Jasmine`,
+		technologies: ["Javascript", "HTML/CSS", "Github", "Jasmine"],
+		link: "https://guillaumedaviid.github.io/Checkers-Game/",
+	},
+
+    {
+        name: "Commercia",
+		title: 'Commercia - site e-commerce',
+		description: "Création d'un e-commerce en React.js.",
+		id: '4',
+		technique: `Développement d'un site e-commerce en React avec Redux comme state manager.
+                    
+        Taches : 
+        - Développement de l'application en React.JS
+        - Utilisation de Redux pour la gestion du State
+        - Déploiement / Hébergement via Vercel (CI/CD)
+        - Test Unitaire React-testing-library`,
+		technologies: ["React.js", "Javascript", "SASS (SCSS)", "Redux", "React Router", "React-Testing-Library / JEST", "CI/CD", "Vercel"],
+		link: "https://commercia.vercel.app/",
+	},
+]
